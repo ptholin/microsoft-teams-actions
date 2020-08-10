19ef69e50c8e8b40ff49b9d6e9c2e7ccca0489f2
Patrik Thölin <patrik@antwork.se>
2020-08-10 17:56:50 +0200

    test
    test

--- src/GitHubActions.Teams.ConsoleApp/Program.cs
+++ src/GitHubActions.Teams.ConsoleApp/Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-
 using CommandLine;
-
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
